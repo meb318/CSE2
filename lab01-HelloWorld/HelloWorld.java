6bc6393834cc82c3b69de98f93e68bfb85e48389
meb318 <meb318@lehigh.edu>
2015-08-28 13:40:16 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+//Max Brungardt
+//Lab1
+//HelloWorld Java Program
+
+//define a class
+public class HelloWorld{
+    
+    //add main method
+    public static void main(String[] args){
+    //print the statment with box around it
+        System.out.println("################");
+        System.out.println("################");
+        System.out.println("#              #");
+        System.out.println("#  HelloWorld  #");
+        System.out.println("#              #");
+        System.out.println("################");
+        System.out.println("################");
+        System.out.println("test");
+        
+        
+        //test/testtest
+    }
+}
