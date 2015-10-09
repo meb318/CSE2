22066ca3ca7ac536490f3cc05211be885f5cda14
meb318 <meb318@lehigh.edu>
2015-10-09 01:05:20 +0000

    updated lab06

--- lab06/timeSheets.java
+++ lab06/timeSheets.java
@@ -3,47 +3,38 @@
 //Then asks for 5 integers saying the hours they worked on each day of
 //the work week. Then print the total payroll of all employees
 
-import java.util.Scanner;
+import java.util.Scanner;//allow user to input data
 
 public class timeSheets{
     public static void main(String[] args){
-        Scanner myScanner = new Scanner(System.in);
-        System.out.print("How many employees (must be positive): ");
-        int employees = myScanner.nextInt();
-        int loopCount = employees - employees;
-        int payAll;
+        Scanner myScanner = new Scanner(System.in);//set user input variable
+        System.out.print("How many employees (must be positive): ");//ask how many employees worked
+        int employees = myScanner.nextInt();//set the number entered to be the number of employees
+        int loopCount = employees - employees;//start a counter for the number of employees
+        int abc;//start the pay all variable
+        int everyone = 0;//setting the counter for pay for everyone
         
-        while (employees > loopCount){
-            System.out.print("Enter pay per hour in cents: ");
-            int pay = myScanner.nextInt();
-            System.out.print("Hours for Monday: ");
-            int mon = myScanner.nextInt();
-            System.out.print("Hours for Tuesday: ");
-            int tues = myScanner.nextInt();
-            System.out.print("Hours for Wednesday: ");
-            int wed = myScanner.nextInt();
-            System.out.print("Hours for Thursday: ");
-            int thurs = myScanner.nextInt();
-            System.out.print("Hours for Friday: ");
-            int fri = myScanner.nextInt();
+        while (employees > loopCount){//tell the loop to run until loopcount is larger than employees
+            System.out.print("Enter pay per hour in cents: ");//ask how much the pay per hour is
+            int pay = myScanner.nextInt();//set the pay per hour variable
+            System.out.print("Hours for Monday: ");//ask for hours on monday
+            int mon = myScanner.nextInt();//set the hours on monday variable 
+            System.out.print("Hours for Tuesday: ");//ask for hours on tuesday
+            int tues = myScanner.nextInt();//set the hours on tuesday variable
+            System.out.print("Hours for Wednesday: ");//ask for hours on wednesday
+            int wed = myScanner.nextInt();//set the hours on wednesday variable
+            System.out.print("Hours for Thursday: ");//ask for hours on thursday
+            int thurs = myScanner.nextInt();//set the hours on thursdy variable
+            System.out.print("Hours for Friday: ");//ask for hours on friday
+            int fri = myScanner.nextInt();//set the hours on friday variable
             
-            int totalHours = mon + tues + wed + thurs + fri;
-            int totalPay = totalHours * pay;
+            int totalHours = mon + tues + wed + thurs + fri;//add all the hours together under one variable
+            int totalPay = totalHours * pay;//find the total pay per worker
             
-            for(employees > loopCount){
-            payAll = totalPay;
-            //Store the total pay for each employee in a new variable
-            //Then add that to payAll
-            //This will destroy the old number each time a new one is made and have a counter
-            //for all the numbers combined
-            }
+            everyone = everyone + totalPay;//adding the current persons pay to the counter for everyones pay
             
-            loopCount += 1;
-            
-            //System.out.print(totalHours);
-            System.out.println("Total for all employees is: $" + payAll);
-            
-            
+            loopCount++;//add to the loop counter
         }
+            System.out.println("The total payroll in cents is:" + everyone);//print out the total pay for everyone in cents
     }
 }
